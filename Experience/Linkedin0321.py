18e1a8540aedb91cbb76b11211d6fbc01fa5392c
UmassJin <jinblue.d@gmail.com>
2015-03-21 14:42:59 -0700

    Update Linkedin0321.py

--- Experience/Linkedin0321.py
+++ Experience/Linkedin0321.py
@@ -1,1 +1,10 @@
+1. K closest points:
+  Find the K closest points to the origin in a 2D plane, given an array containing N points.
+2.Maximum Sum/Production Subarray 
+3.Pow(x,n)
+4 Text Justification
+5.Valid Number
+6.Implement List/Stack/MaP
+7.Isomorphic
+8.Implement Read/Write Lock for HashMap/Buffer/etc
 
