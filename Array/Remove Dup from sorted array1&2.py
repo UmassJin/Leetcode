aa235cc446fcc708e7f7dc895b1f9d511a255838
UmassJin <jinblue.d@gmail.com>
2015-03-21 13:32:40 -0700

    Update and rename Remove Dup from sorted array.py to Remove Dup from sorted array1&2.py

--- Array/Remove Dup from sorted array1&2.py
+++ Array/Remove Dup from sorted array1&2.py
@@ -29,3 +29,27 @@ What if duplicates are allowed at most twice?
 For example,
 Given sorted array A = [1,1,1,2,2,3],
 Your function should return length = 5, and A is now [1,1,2,2,3].
+Analysis: Use a flag to indicate the count 
+
+class Solution:
+    # @param A a list of integers
+    # @return an integer
+    def removeDuplicates(self, A):
+        return self.removeDupk(A,2)
+        
+    def removeDupk(self,A, k): 
+        length = len(A)
+        if length <= k: return length
+        
+        slow = 0; count = 1
+        for fast in xrange(1,length):
+            if A[fast] == A[fast-1]:
+                if count < k:
+                    slow +=1
+                    A[slow] = A[fast]
+                    count += 1
+            else:
+                slow += 1
+                A[slow] = A[fast]
+                count = 1
+        return slow+1
