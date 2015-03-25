8d45c39f82f870bb7313b5af0def0416646efecf
UmassJin <jinblue.d@gmail.com>
2015-03-24 22:29:45 -0700

    Create Palindrome_PartitioningII.py

---
+++ Array/Palindrome_PartitioningII.py
@@ -0,0 +1,8 @@
+Given a string s, partition s such that every substring of the partition is a palindrome.
+Return the minimum cuts needed for a palindrome partitioning of s.
+For example, given s = "aab",
+Return 1 since the palindrome partitioning ["aa","b"] could be produced using 1 cut.
+
+Reference:
+https://leetcode.com/discuss/9476/solution-does-not-need-table-palindrome-right-uses-only-space
+http://yucoding.blogspot.com/2013/08/leetcode-question-133-palindrome.html
