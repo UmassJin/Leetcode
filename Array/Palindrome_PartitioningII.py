089f64410b50234672be4e7bc33a29b2e76770ff
UmassJin <jinblue.d@gmail.com>
2015-03-25 12:11:23 -0700

    Update Palindrome_PartitioningII.py

--- Array/Palindrome_PartitioningII.py
+++ Array/Palindrome_PartitioningII.py
@@ -3,10 +3,10 @@ Return the minimum cuts needed for a palindrome partitioning of s.
 For example, given s = "aab",
 Return 1 since the palindrome partitioning ["aa","b"] could be produced using 1 cut.
 
-Reference:
-https://leetcode.com/discuss/9476/solution-does-not-need-table-palindrome-right-uses-only-space
-http://yucoding.blogspot.com/2013/08/leetcode-question-133-palindrome.html
-http://chaoren.is-programmer.com/posts/43805
+#Reference:
+#https://leetcode.com/discuss/9476/solution-does-not-need-table-palindrome-right-uses-only-space
+#http://yucoding.blogspot.com/2013/08/leetcode-question-133-palindrome.html
+#http://chaoren.is-programmer.com/posts/43805
 
 class Solution:
     # @param s, a string
