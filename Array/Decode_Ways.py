069ba86d09239c218c193f0d67e9a5e5ddb9d84e
UmassJin <jinblue.d@gmail.com>
2015-03-25 17:27:24 -0700

    Update Decode_Ways.py

--- Array/Decode_Ways.py
+++ Array/Decode_Ways.py
@@ -8,6 +8,15 @@ For example,
 Given encoded message "12", it could be decoded as "AB" (1 2) or "L" (12).
 The number of ways decoding "12" is 2.
 
+# Note:
+# State: dp[i] means from char 0 to char i-1 how many decode ways
+# Initialize: dp[0]=1,dp[1]=1
+# Function: 
+# 1) 当s[i-2:i]这两个字符是10~26但不包括10和20这两个数时，比如21，那么可以有两种编码方式（BA，U），所以dp[i]=dp[i-1]+dp[i-2]
+# 2) 当s[i-2:i]等于10或者20时，由于10和20只有一种编码方式，所以dp[i]=dp[i-2]
+# 3) 当s[i-2:i]不在以上两个范围时，如09这种，编码方式为0，而31这种，dp[i]=dp[i-1]。
+# Result: dp[length]
+
 class Solution:
     # @param s, a string
     # @return an integer
