f8a5c0be8df2381cb46b72619d124847d96d201d
UmassJin <jinblue.d@gmail.com>
2015-03-27 18:36:24 -0700

    Update Anagrams.py

--- Array/Anagrams.py
+++ Array/Anagrams.py
@@ -25,3 +25,24 @@ class Solution:
                 result.extend(dict[key])  # Note: Here not use the append, use extend 
         
         return result 
+
+class Solution:
+    # @param strs, a list of strings
+    # @return a list of strings
+    def anagrams(self, strs):
+        d = {}
+        for s in strs:
+            key = ''.join(sorted(s))
+            d.setdefault(key,[]).append(s)
+        ret = []
+        for key in d:
+            if len(d[key]) > 1:
+                ret.extend(d[key])
+        return ret
+    # Note:
+    # 1. Need to use extend here, return those len(d[key]) > 1
+    # 2. Need to remember the definition of Anagrams
+
+    Input:      ["tea","and","ate","eat","dan"]
+    Output:     ["and","dan"]
+    Expected:   ["and","dan","tea","ate","eat"]
