1c6c5155d3f14728dee8c5a5685f9ee4af4bf33e
UmassJin <jinblue.d@gmail.com>
2015-03-29 15:40:19 -0700

    Create Count_and_Say.py

---
+++ Array/Count_and_Say.py
@@ -0,0 +1,33 @@
+The count-and-say sequence is the sequence of integers beginning as follows:
+1, 11, 21, 1211, 111221, ...
+
+1 is read off as "one 1" or 11.
+11 is read off as "two 1s" or 21.
+21 is read off as "one 2, then one 1" or 1211.
+Given an integer n, generate the nth sequence.
+
+Note: The sequence of integers will be represented as a string.
+
+class Solution:
+    # @return a string
+    def countAndSay(self, n):
+        s = '1'
+        for i in xrange(1,n):  # Note: here start from 1 
+            s = self.count(s)
+        return s 
+    
+    def count(self,s):
+        amount = 1
+        curr = '#'
+        result = ''
+        for char in s:
+            if char != curr:
+                if curr != '#':
+                    result += str(amount) + curr  # Note: here we should use result += str(amount) + curr
+                curr = char
+                amount = 1
+            else:
+                amount +=1
+        result += str(amount) + curr
+        return result 
+
