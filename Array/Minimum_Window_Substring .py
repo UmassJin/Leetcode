414a6e12414cdce0c23be9992506c36d02f84a5c
UmassJin <jinblue.d@gmail.com>
2015-04-01 13:48:31 -0700

    Update Minimum_Window_Substring .py

--- Array/Minimum_Window_Substring .py
+++ Array/Minimum_Window_Substring .py
@@ -11,3 +11,43 @@ If there is no such window in S that covers all characters in T, return the emtp
 If there are multiple such windows, you are guaranteed that there will always be only one unique minimum window in S.
 
 # Reference: http://www.geeksforgeeks.org/find-the-smallest-window-in-a-string-containing-all-characters-of-another-string/
+
+class Solution:
+    # @return a string
+    def minWindow(self, S, T):
+        lengths = len(S)
+        lengtht = len(T)
+        if lengtht > lengths: return ""
+        
+        required = {}
+        found = {}
+        result = ''
+        count = 0  # calculate whether there is enough char in T present in S 
+        l = 0
+        
+        for char in T:
+            required[char] = required.get(char,0) + 1
+            found[char] = 0
+        
+        for r in xrange(lengths):
+            if S[r] not in required:
+                continue 
+            
+            found[S[r]] += 1
+            if found[S[r]] <= required[S[r]]: 
+                count += 1
+            
+            if count == lengtht:
+                while l < r:
+                    if S[l] not in required:
+                        l += 1
+                        continue
+                    if found[S[l]] > required[S[l]]:
+                        found[S[l]] -= 1
+                        l += 1
+                        continue 
+                    break
+                if len(result) == 0 or len(result) > (r-l+1): 
+                    result = S[l:r+1]
+        
+        return result 
