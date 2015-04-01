8d9ae983092185f04af50e3450fb5bb7ad6df998
UmassJin <jinblue.d@gmail.com>
2015-04-01 12:08:47 -0700

    Create Minimum_Window_Substring .py

---
+++ Array/Minimum_Window_Substring .py
@@ -0,0 +1,13 @@
+Given a string S and a string T, find the minimum window in S which will contain all the characters in T in complexity O(n).
+
+For example,
+S = "ADOBECODEBANC"
+T = "ABC"
+Minimum window is "BANC".
+
+Note:
+If there is no such window in S that covers all characters in T, return the emtpy string "".
+
+If there are multiple such windows, you are guaranteed that there will always be only one unique minimum window in S.
+
+# Reference: http://www.geeksforgeeks.org/find-the-smallest-window-in-a-string-containing-all-characters-of-another-string/
