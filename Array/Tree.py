4244f950452190b47f9eb2a5bb38e85ebf3c41fa
UmassJin <jinblue.d@gmail.com>
2015-04-06 18:43:53 -0700

    Update Tree.py

--- Array/Tree.py
+++ Array/Tree.py
@@ -650,3 +650,23 @@ class Solution:
         return
 
 # 33) Flatten Binary Tree to Linked List 
+class Solution:
+    # @param root, a tree node
+    # @return nothing, do it in place
+    def flatten(self, root):
+        if (root == None):
+            return
+        
+        self.flatten(root.left) 
+        self.flatten(root.right)
+        
+        p = root
+        if p.left == None:
+            return
+        p = p.left
+        while p.right:
+            p = p.right
+        p.right = root.right 
+        root.right = root.left
+        root.left = None
+        
