068681dbff52ceb69ad3251db345e3326444e118
UmassJin <jinblue.d@gmail.com>
2015-04-08 21:36:33 -0700

    Update Number_of_Islands.py

--- Array/Number_of_Islands.py
+++ Array/Number_of_Islands.py
@@ -1,3 +1,4 @@
+'''
 Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
 
 Example 1:
@@ -15,7 +16,7 @@ Example 2:
 00100
 00011
 Answer: 3
-
+'''
 
 class Solution:
     # @param grid, a list of list of characters
