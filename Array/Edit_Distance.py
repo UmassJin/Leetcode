e8965afd910a2c13471782e2d95ce5186becd238
UmassJin <jinblue.d@gmail.com>
2015-05-12 16:13:57 -0700

    Update Edit_Distance.py

--- Array/Edit_Distance.py
+++ Array/Edit_Distance.py
@@ -25,7 +25,7 @@ class Solution:
                     dp[0][j] = j
                 elif j == 0:
                     dp[i][0] = i
-                elif word1[i-1] == word2[j-1]:
+                elif word1[i-1] == word2[j-1]: # 判断 i-1 和 j-1 !! 不是 i 和 j
                     dp[i][j] = dp[i-1][j-1]
                 else:
                     dp[i][j] = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1])+1
