c35a608b0def6f393c213cbf23f38c44f7c26584
UmassJin <jinblue.d@gmail.com>
2015-05-15 14:35:02 -0700

    Update Pascal's Triangle 1&2.py

--- Array/Pascal's Triangle 1&2.py
+++ Array/Pascal's Triangle 1&2.py
@@ -11,30 +11,6 @@ Return
   [1,3,3,1],
  [1,4,6,4,1]
 ]
-
-class Solution:
-    # @return a list of lists of integers
-    # Method 1 
-    def generate(self, numRows):
-        if numRows == 0:
-            return []
-        if numRows == 1:
-            return [[1]]
-        result = [[1],[1,1]]
-        if numRows == 2:
-            return result
-        for i in xrange(2,numRows):
-            pre_list = result[i-1]
-            current_list = [1]
-            for j in xrange(1, i):
-                current_list.append(pre_list[j-1] + pre_list[j])
-                if j == i-1:
-                    current_list.append(1)
-                    break
-            result.append(current_list)
-        return result     
- 
-     # Method 2 
      def generate(self, numRows):
          result = []
          for i in xrange(numRows):
@@ -55,9 +31,23 @@ class Solution:
     # @return a list of integers
     def getRow(self, rowIndex):
         result = []
-        for i in xrange(rowIndex+1):
+        for i in xrange(rowIndex+1):   
             result.append([1]*(i+1))
             if i > 1:
                 for j in xrange(1,i):
                     result[i][j] = result[i-1][j] + result[i-1][j-1]
         return result[rowIndex]
+
+
+class Solution:
+    # @param {integer} rowIndex
+    # @return {integer[]}
+    def getRow(self, rowIndex):
+        pre = []
+        for i in xrange(rowIndex + 1):  # Note: here is rowIndex + 1
+            line = [1]*(i+1)
+            if i > 1:
+                for j in xrange(1, i):
+                    line[j] = pre[j-1] + pre[j]
+            pre = line[:]
+        return pre
