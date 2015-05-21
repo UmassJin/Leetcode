f38b0f8f8584584d0d5b2e4ab745497b0f581c4c
UmassJin <jinblue.d@gmail.com>
2015-05-21 10:09:48 -0700

    Update Tree.py

--- Array/Tree.py
+++ Array/Tree.py
@@ -806,3 +806,41 @@ class Solution:
         root.left = self.sortedListToBST(head)
         root.right = self.sortedListToBST(right)
         return root 
+
+# Binary Search Tree Iterator
+# Definition for a  binary tree node
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class BSTIterator:
+    # @param root, a binary search tree's root node
+    def __init__(self, root):
+        self.root = root 
+        self.q = []
+        self.getleft(self.root)
+        
+    # @return a boolean, whether we have a next smallest number
+    def hasNext(self):
+        if not self.q:
+            return False
+        else:
+            return True
+
+    # @return an integer, the next smallest number
+    def next(self):
+       node = self.q.pop()
+       self.getleft(node.right)
+       return node.val
+        
+    def getleft(self, node):
+        while node:
+            self.q.append(node)
+            node = node.left
+            
+
+# Your BSTIterator will be called like this:
+# i, v = BSTIterator(root), []
+# while i.hasNext(): v.append(i.next())
