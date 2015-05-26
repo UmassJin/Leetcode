9ecf9b1688280e1d81d30bce27b90a2e8c765345
UmassJin <jinblue.d@gmail.com>
2015-05-25 21:31:21 -0700

    Update Create_Largest_Number.py

--- Array/Create_Largest_Number.py
+++ Array/Create_Largest_Number.py
@@ -14,9 +14,10 @@ class Solution:
     # @return a string
     def largestNumber(self, num):
         num = sorted([str(x) for x in num], cmp = self.compare)
-        ans = ''.join(num).lstrip('0')
-        return ans or '0'
+        ans = ''.join(num).lstrip('0') # 注意要去掉头部的'0'
+        return ans or '0'  # 注意这里可能返回 '0'
     
     def compare(self, a, b):
-        return [1,-1] [a + b > b + a]
+        return [1,-1] [a + b > b + a] 
+        # 这里是比较'12' + '21' > '21' + '12'，如果a+b > b+a, 返回-1，a< b, a放在b前面，否则返回-1
      
