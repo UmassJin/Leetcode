4c2868bb9ae3577f0b8f7b8ac78a9954fbd94e75
UmassJin <jinblue.d@gmail.com>
2015-05-25 20:33:10 -0700

    Update Best Time to Buy and Sell Stock IV.py

--- Array/Best Time to Buy and Sell Stock IV.py
+++ Array/Best Time to Buy and Sell Stock IV.py
@@ -42,5 +42,72 @@ class Solution:
 # and buying the stock at price[j] - this is used for the next loop.
 
 
+# Method 2
+class Solution:
+    # @return an integer as the maximum profit 
+    def maxProfit_1(self, k, prices):
+         if k == 0 or len(prices) < 2: return 0
+         if k >= len(prices)//2:
+             return sum((i-j) for i, j in zip(prices[1:],prices[:-1]) if i>j)
+
+         hold,release = [float('-inf')]*(k+1), [0]*(k+1)
+
+         for p in prices:
+             for i in range(1,k+1):
+                 release[i] = max(release[i],hold[i]+p)
+                 hold[i] = max(hold[i], release[i-1]-p)
+                 print "p: %d, i: %d" %(p, i)
+                 print "release: ", release
+                 print "hold: ", hold
+         return release[k]
+
+'''
+test case: 
+test = Solution()
+print test.maxProfit(2, [1,7,2,8,3,9])
+
+[JINZH2-M-20GQ: ~/Desktop/Python_training/Leetcode]: python stockIV.py 
+p: 1, i: 1
+release:  [0, 0, 0]
+hold:  [-inf, -1, -inf]
+p: 1, i: 2
+release:  [0, 0, 0]
+hold:  [-inf, -1, -1]
+p: 7, i: 1
+release:  [0, 6, 0]
+hold:  [-inf, -1, -1]
+p: 7, i: 2
+release:  [0, 6, 6]
+hold:  [-inf, -1, -1]
+p: 2, i: 1
+release:  [0, 6, 6]
+hold:  [-inf, -1, -1]
+p: 2, i: 2
+release:  [0, 6, 6]
+hold:  [-inf, -1, 4]
+p: 8, i: 1
+release:  [0, 7, 6]
+hold:  [-inf, -1, 4]
+p: 8, i: 2
+release:  [0, 7, 12]
+hold:  [-inf, -1, 4]
+p: 3, i: 1
+release:  [0, 7, 12]
+hold:  [-inf, -1, 4]
+p: 3, i: 2
+release:  [0, 7, 12]
+hold:  [-inf, -1, 4]
+p: 9, i: 1
+release:  [0, 8, 12]
+hold:  [-inf, -1, 4]
+p: 9, i: 2
+release:  [0, 8, 13]
+hold:  [-inf, -1, 4]
+13
+'''
+
+# release[i] 表示第i次以价格p卖出赚多少钱
+# hold[i] 表示第第i次以价格p买入还剩多少钱，给下一次卖出用
+
 
 # Reference: https://leetcode.com/discuss/25603/a-concise-dp-solution-in-java
