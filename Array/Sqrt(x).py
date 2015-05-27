49066614b742afb0bc2bec4b8b0f26ee668b2c32
UmassJin <jinblue.d@gmail.com>
2015-05-27 14:36:21 -0700

    Update Sqrt(x).py

--- Array/Sqrt(x).py
+++ Array/Sqrt(x).py
@@ -8,6 +8,7 @@ Compute and return the square root of x.
 #很有可能是使用二分查找来解决问题的。这里要注意折半查找起点和终点的设置。起点i=1；终点j=x/2+1；
 # 因为在(x/2+1)^2 > x，所以我们将折半查找的终点设为x/2+1。
 
+# Solution 1: 
 class Solution:
     # @param {integer} x
     # @return {integer}
@@ -27,6 +28,24 @@ class Solution:
                 l = mid + 1
         return r
 
+# Solution 2:
+    def mySqrt(self, x):
+        if x < 0: return -1
+        if x == 0: return 0
+        l = 1; r = x/2+1 
+        
+        while l <= r:
+            m = (l+r)/2
+            
+            if x >= m*m and x < (m+1)*(m+1):
+                return m
+            
+            if x < m*m:
+                r = m-1
+                
+            else:
+                l = m+1
+        return 0
 
 # Reference: http://www.cnblogs.com/zuoyuan/p/3775852.html
 # Reference: http://blog.csdn.net/linhuanmars/article/details/20089131
