714ca5d64b0f4f506c9a69cf506c967ffc210e56
UmassJin <jinblue.d@gmail.com>
2015-06-01 12:05:08 -0700

    Create Segment Tree Modify.py

---
+++ LintCode/Segment Tree Modify.py
@@ -0,0 +1,66 @@
+'''
+For a Maximum Segment Tree, which each node has an extra value max to store the maximum value in this node's interval.
+
+Implement a modify function with three parameter root, index and value to change the node's value with [start, end] = [index, index] to the new given value. Make sure after this change, every node in segment tree still has the max attribute with the correct value.
+
+Have you met this question in a real interview? Yes
+Example
+For segment tree:
+
+                      [1, 4, max=3]
+                    /                \
+        [1, 2, max=2]                [3, 4, max=3]
+       /              \             /             \
+[1, 1, max=2], [2, 2, max=1], [3, 3, max=0], [4, 4, max=3]
+if call modify(root, 2, 4), we can get:
+
+                      [1, 4, max=4]
+                    /                \
+        [1, 2, max=4]                [3, 4, max=3]
+       /              \             /             \
+[1, 1, max=2], [2, 2, max=4], [3, 3, max=0], [4, 4, max=3]
+or call modify(root, 4, 0), we can get:
+
+                      [1, 4, max=2]
+                    /                \
+        [1, 2, max=2]                [3, 4, max=0]
+       /              \             /             \
+[1, 1, max=2], [2, 2, max=1], [3, 3, max=0], [4, 4, max=0]
+Note
+We suggest you finish problem Segment Tree Build and Segment Tree Query first.
+
+Challenge
+Do it in O(h) time, h is the height of the segment tree.
+'''
+
+
+"""
+Definition of SegmentTreeNode:
+class SegmentTreeNode:
+    def __init__(self, start, end, max):
+        this.start, this.end, this.max = start, end, max
+        this.left, this.right = None, None
+"""
+
+class Solution:	
+    """
+    @param root, index, value: The root of segment tree and 
+    @ change the node's value with [index, index] to the new given value
+    @return: nothing
+    """
+    def modify(self, root, index, value):
+        self.modify_helper(root, index, value)
+    
+    def modify_helper(self, root, index, value):
+        if root.start > index or root.end < index: 
+                return root.max
+        if index == root.start and index == root.end:
+            root.max = value
+            
+        if root.left and root.right:
+            left = self.modify_helper(root.left, index, value)
+            right = self.modify_helper(root.right, index, value)
+            root.max = max(left, right)
+        
+        return root.max 
+        
