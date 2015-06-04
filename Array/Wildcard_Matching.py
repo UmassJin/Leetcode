b20ee72b8057913f564f119d93f2d4059d7bfab5
UmassJin <jinblue.d@gmail.com>
2015-06-04 09:56:52 -0700

    Update Wildcard_Matching.py

--- Array/Wildcard_Matching.py
+++ Array/Wildcard_Matching.py
@@ -55,3 +55,39 @@ class Solution:
 
 # Reference: [Yu's Garden] http://www.cnblogs.com/yuzhangcmu/p/4116153.html
 # [九章算法答案] http://www.jiuzhang.com/solutions/wildcard-matching/
+# [Other good solution] http://blog.csdn.net/abcbc/article/details/8868711
+
+
+# DP solution: 
+# Status: dp[i][j]: the matching True/False for s[:i-1] and p[:j-1]
+# Initialization:    dp[0][0] = True
+#                    dp[0][j] = (p[j-1] == '*' and dp[0][j-1])
+# Function: dp[i][j] = dp[i-1][j-1] and s[i-1] == p[j-1]     if p[j-1] != '?' and != '*'
+#                      dp[i-1][j-1]                          if p[j-1] == '?'
+#                      dp[i][j-1] || dp[i-1][j-1] || dp[i-2][j-1] || ... || dp[0][j-1] = dp[i][j-1] || dp[i-1][j] if p[j-1] == '*'
+class Solution:
+    """
+    @param s: A string 
+    @param p: A string includes "?" and "*"
+    @return: A boolean
+    """
+    def isMatch(self, s, p):
+        lens = len(s)
+        lenp = len(p)
+        
+        dp = [[False for i in xrange(lenp+1)] for j in xrange(lens+1)]
+        dp[0][0] = True
+        
+        for i in xrange(1,lenp+1):
+            dp[0][i] = (dp[0][i-1] and p[i-1] == '*')
+        
+        for i in xrange(1, lens+1):
+            for j in xrange(1, lenp+1):
+                if p[j-1] != '*' and p[j-1] != '?':
+                    dp[i][j] = (dp[i-1][j-1] and (s[i-1] == p[j-1]))
+                elif p[j-1] == '?':
+                    dp[i][j] = dp[i-1][j-1]
+                elif p[j-1] == '*':
+                    dp[i][j] = (dp[i][j-1] or dp[i-1][j])
+                    
+        return dp[lens][lenp]
