20cd580d77434ebe3656e4669073cb4c2f05ad63
UmassJin <jinblue.d@gmail.com>
2015-06-04 15:44:00 -0700

    Update 3Sum.py

--- Array/3Sum.py
+++ Array/3Sum.py
@@ -19,20 +19,21 @@ class Solution:
             j = i + 1
             k = len(num) - 1
             
-            if i==0 or num[i] > num[i-1]:
+            if i==0 or num[i] > num[i-1]:  # optimization 
               while j < k:
                 sum = num[i] + num[j] + num[k]
                 if sum == 0:
-                    result.append([num[i],num[j],num[k]])
+                    if (numbers[i], numbers[j], numbers[k]) not in result:
+                        result.append((numbers[i], numbers[j], numbers[k]))   # 注意duplicate result 
                     j = j + 1
                     k = k - 1
-                    while (j<k and (num[j] == num[j-1])): j +=1
+                    while (j<k and (num[j] == num[j-1])): j +=1 # add the check and reduce the running time 
                     while (j<k and (num[k] == num[k+1])): k -=1
                     
                 elif sum < 0:
                     while j<k:
                         j = j +1
-                        if num[j] > num[j-1]: break
+                        if num[j] > num[j-1]: break  # optimization 
                 else:
                     while j<k:
                         k = k-1
