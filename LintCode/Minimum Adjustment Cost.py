99746635fc6a7f4bfaa35c9df9ced2a07fddd187
UmassJin <jinblue.d@gmail.com>
2015-06-03 17:35:34 -0700

    Create Minimum Adjustment Cost.py

---
+++ LintCode/Minimum Adjustment Cost.py
@@ -0,0 +1,19 @@
+'''
+Given an integer array, adjust each integers so that the difference of every adjacent integers are not greater than a given number target.
+
+If the array before adjustment is A, the array after adjustment is B, you should minimize the sum of |A[i]-B[i]|
+
+Have you met this question in a real interview? Yes
+Example
+Given [1,4,2,3] and target = 1, one of the solutions is [2,3,2,3], the adjustment cost is 2 and it's minimal.
+
+Return 2.
+
+Note
+You can assume each number in the array is a positive integer and not greater than 100.
+'''
+
+
+
+# Reference: http://www.cnblogs.com/yuzhangcmu/p/4153927.html
+# https://github.com/algorhythms/LintCode/blob/master/Minimum%20Adjustment%20Cost.py
