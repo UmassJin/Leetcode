f94717900b8973d597f548502a576ce58f3b68d8
UmassJin <jinblue.d@gmail.com>
2015-06-05 17:19:45 -0700

    Update Convert_Sorted_Linkedlist_to_BST.py

--- Array/Convert_Sorted_Linkedlist_to_BST.py
+++ Array/Convert_Sorted_Linkedlist_to_BST.py
@@ -34,7 +34,52 @@ class Solution:
         root.left = self.sortedListToBST(head)
         root.right = self.sortedListToBST(right)
         return root 
-
+# Time complexity: T(n) = 2T(n/2) + O(n) = O(nlogn)
 # test case: [0], only one node
 # test case: [], empty linked list
 # test case: [1,2], two nodes 
+
+"""
+Definition of ListNode
+class ListNode(object):
+
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        this.val = val
+        this.left, this.right = None, None
+"""
+class Solution:
+    """
+    @param head: The first node of linked list.
+    @return: a tree node
+    """
+    def get_size(self, head):
+        self.current = head 
+        size = 0
+        while head:
+            size += 1
+            head = head.next 
+        return size 
+    
+    def sortedListToBST(self, head):
+        if not head: return head
+        size = self.get_size(head)
+        return self.sortedListToBST_helper(size)
+    
+    def sortedListToBST_helper(self,size):
+        if size <= 0: return None
+        left = self.sortedListToBST_helper(size/2)
+        
+        root = TreeNode(self.current.val)
+        self.current = self.current.next 
+        
+        right = self.sortedListToBST_helper(size - 1 - size/2)
+        root.left = left
+        root.right = right
+        return root 
+
+# Here we get the size at first, then the time complexity is T(n) = 2T(n/2) + O(1)--> O(n) + O(logn) = O(n)
