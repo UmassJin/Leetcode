6ca7b8cc43b046f900df1d9d8622076f1b4b2aa6
UmassJin <jinblue.d@gmail.com>
2015-06-24 09:05:59 -0700

    Update Gas_Station.py

--- Array/Gas_Station.py
+++ Array/Gas_Station.py
@@ -47,3 +47,16 @@ class Solution:
             i += j + 1
         return -1
  
+'''
+1. 几点注意的地方
+a. 从i开始，j是当前station的指针，sum += gas[j] – cost[j] （从j站加了油，再算上从i开始走到j剩的油，走到j+1站还能剩下多少油）
+b.如果sum < 0，说明从i开始是不行的。那能不能从i..j中间的某个位置开始呢？既然i出发到i+1是可行的， 又i~j是不可行的， 从而发现i+1~ j是不可行的。
+c. 以此类推i+2~j， i+3~j，i+4~j 。。。。等等都是不可行的
+d. 所以一旦sum<0，index就赋成j + 1，sum归零。
+e. 最后total表示能不能走一圈。
+
+倘若在i处gas[i]-cost[i] < 0, 则从 i + 1 开始
+倘若total (gas[i] - cost[i]) < 0, 则一定不存在起始点
+
+# Reference: http://www.cnblogs.com/yuzhangcmu/p/4179228.html
+'''
