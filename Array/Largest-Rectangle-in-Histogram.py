d91f38a4485efc6ddeba6c30299c8fa6eb486f21
UmassJin <jinblue.d@gmail.com>
2015-06-25 12:21:51 -0700

    Update Largest-Rectangle-in-Histogram.py

--- Array/Largest-Rectangle-in-Histogram.py
+++ Array/Largest-Rectangle-in-Histogram.py
@@ -10,6 +10,14 @@ return 10.
 #[G4G Set 2 O(n)](http://www.geeksforgeeks.org/largest-rectangle-under-histogram/)
 #[G4G Set 1 O(nlogn)](http://www.geeksforgeeks.org/largest-rectangular-area-in-a-histogram-set-1/)
 
+'''
+For every bar ‘x’, we calculate the area with ‘x’ as the smallest bar in the rectangle. If we calculate such area for every bar ‘x’ and find the maximum of all areas, 
+our task is done. How to calculate area with ‘x’ as smallest bar? We need to know index of the first smaller (smaller than ‘x’) bar on left of ‘x’ and index of first 
+smaller bar on right of ‘x’. Let us call these indexes as ‘left index’ and ‘right index’ respectively.
+We traverse all bars from left to right, maintain a stack of bars. Every bar is pushed to stack once. A bar is popped from stack when a bar of smaller height is seen. 
+When a bar is popped, we calculate the area with the popped bar as smallest bar. How do we get left and right indexes of the popped bar – the current index tells us the 
+‘right index’ and index of previous item in stack is the ‘left index’.
+'''
 
 class Solution:
     # @param height, a list of integer
@@ -22,7 +30,7 @@ class Solution:
         maxarea = 0
         i = 0
         while i < n: # 注意这里用while，不用for，否则会跳过一个
-            if len(stack) == 0 or height[i] > height[stack[-1]]:
+            if len(stack) == 0 or height[i] > height[stack[-1]]:  #注意这里不是比较height[i] > height[i-1]!
                 stack.append(i)
                 i += 1  
                 # Note: we only need to i += 1 here! we do not need to add 1 in the else case 
