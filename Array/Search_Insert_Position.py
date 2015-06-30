4afcf9784e8ec6611f0eea9c64f20c57058366b6
UmassJin <jinblue.d@gmail.com>
2015-06-29 21:00:18 -0700

    Update Search_Insert_Position.py

--- Array/Search_Insert_Position.py
+++ Array/Search_Insert_Position.py
@@ -9,21 +9,21 @@ Here are few examples.
 [1,3,5,6], 0 → 0
 
 class Solution:
-    # @param A, a list of integers
-    # @param target, an integer to be inserted
-    # @return integer
-    def searchInsert(self, A, target):
-        start = 0; end = len(A)-1
-        if len(A) == 0: return None
+    # @param {integer[]} nums
+    # @param {integer} target
+    # @return {integer}
+    def searchInsert(self, nums, target):
+        left = 0; right = len(nums) - 1
+        while left <= right:
+            mid = (left + right) / 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                right = mid -1
+            else:
+                left = mid + 1
         
-        while start< len(A) and end >= 0 and start <= end:
-            mid = (start+end)/2
-            if A[mid] == target: return mid
-            elif A[mid] > target: 
-                    end = mid -1
-            elif A[mid] < target:
-                    start = mid + 1
-        return start
+        return left 
 
 # Note: the while loop condition !
 # Last the return value is start !
