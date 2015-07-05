89ea2650296a98870d2fbed8f5a90bfef61e794f
UmassJin <jinblue.d@gmail.com>
2015-07-05 11:32:27 -0700

    Update facebook面经集合.py

--- Experience/facebook面经集合.py
+++ Experience/facebook面经集合.py
@@ -42,4 +42,86 @@ print sort_colors(matrix)
 就没戏了，design面这么烂基本是硬伤，弥补不了了。
 '''
 
+'''
+4
+有这么一个class Contact，里面有一个string的name，和一个vector 装着email address，是这个Contact有的address，用一个list装着是因为一个人有可能有多个email，现在给你vector，比如
+{
+    { "Johhhn", {"john@gmail.com"} },
+    { "Mary", {"mary@gmail.com"} },
+    { "John", {"john@yahoo.com"} },
+    { "John", {"john@gmail.com", "john@yahoo.com", "john@hotmail.com"} },
+    { "Bob",  {"bob@gmail.com"} }
+}
+
+现在我们知道如果email address相同的话，那么就说明是同一个人，现在要做的是根据这些email address，
+把同一个contact给group起来. 比如我们就可以知道#1，#3，#4是同一个人。注意不能根据名字来group，因为有可
+能有相同名字的人，或者同一个人有可能有不同的名字来注册之类的。
+
+* Reference: http://www.fgdsb.com/tags/Union-Find/
+'''
+
+class Contact:
+    def __init__(self, name, email):
+        self.name = name
+        self.emails = email # list of emails 
+
+class UnionFind:
+    def __init__(self, n):
+        self.father = [i for i in xrange(n)]
+        self.ranks = [0 for i in xrange(n)]
+
+    def find(self, x):
+        if self.father[x] == x:
+            return x
+        return find(self.father[x])
+
+    def do_union(self, x, y):
+        x = self.find(x)
+        y = self.find(y)
+        if x == y: return
+        if self.ranks[x] < self.ranks[y]:
+            self.father[x] = y
+        else:
+            self.father[y] = x
+            if self.ranks[x] == self.ranks[y]:
+                self.ranks[x] += 1
+                
+class Solution:
+    def group_contacts(self, contact):
+        n = len(contact)
+        uf = UnionFind(n)
+        email_map = {}
+        # first build the map like 
+        # {"john@gmail.com":[0,3],
+        #  "mary@gmail.com": [1],
+        #  "john@yahoo.com": [2,3],
+        #  "john@hotmail.com": [3],
+        #  "bob@gmail.com": [4]}
+        for i in xrange(n):
+            for email in contact[i].emails:
+                email_map.setdefault(email, []).append(i)
+        
+        # then for each list in each email, join them into one group
+        # so father init: [0,1,2,3,4]
+        # after [0,1,0,0,4]
+        for p in email_map:
+            for j in xrange(len(email_map[p])-1):
+                uf.do_union(j, j+1)
+
+        # Then group for each root, create a list
+        # {0: [0,2,3],
+        #  1, [1],
+        #  4, [4]}
+        group = {}
+        for i in xrange(n):
+            group.setdefault(uf.find(i),[]).append(i)
+
+        result = []
+        for p in group:
+            rec = []
+            for i in group[p]:
+                rec.append(contact[i])
+            result.append(rec)
+        return result
+
 
