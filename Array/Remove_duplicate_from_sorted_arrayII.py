75755b12772f32187f977e6b72cb84160401bf43
UmassJin <jinblue.d@gmail.com>
2015-07-07 21:54:41 -0700

    Update Remove_duplicate_from_sorted_arrayII.py

--- Array/Remove_duplicate_from_sorted_arrayII.py
+++ Array/Remove_duplicate_from_sorted_arrayII.py
@@ -8,6 +8,27 @@ Given sorted array nums = [1,1,1,2,2,3],
 Your function should return length = 5, with the first five elements of nums being 1, 1, 2, 2 and 3. It doesn't matter what you leave beyond the new length.
 '''
 
+# https://leetcode.com/discuss/42348/3-6-easy-lines-c-java-python-ruby
+def removeDuplicates(self, nums):
+    i = 0
+    for n in nums:
+        if i < 2 or n > nums[i-2]:
+            nums[i] = n
+            i += 1
+    return i
+    
+# https://leetcode.com/discuss/42340/python-lines-extra-variables-76ms-any-simpler-solution-else
+def removeDuplicates(self, nums):
+        if len(nums) < 3: 
+            return len(nums)
+        pos = 1
+        for i in range(1, len(nums)-1):
+            if nums[i-1] != nums[i+1]:
+                nums[pos] = nums[i]
+                pos += 1
+        nums[pos] = nums[-1]
+        return pos + 1    
+
 
 class Solution:
     # @param {integer[]} nums
