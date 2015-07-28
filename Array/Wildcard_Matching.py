05b8e9baa1506c9fe6ad76d56725db2779f92659
UmassJin <jinblue.d@gmail.com>
2015-07-27 21:13:09 -0700

    Update Wildcard_Matching.py

--- Array/Wildcard_Matching.py
+++ Array/Wildcard_Matching.py
@@ -58,3 +58,45 @@ class Solution:
 
 # DP solution
 # https://leetcode.com/discuss/26399/python-dp-solution
+
+
+# https://leetcode.com/discuss/26399/python-dp-solution
+public boolean isMatch(String s, String p) {
+    int count = 0;
+    for (char c : p.toCharArray()) {
+        if (c == '*')
+            count++;
+    }
+    if (p.length() - count > s.length())
+        return false;
+    boolean[][] dp = new boolean[p.length() + 1][s.length() + 1];
+    dp[0][0] = true;
+    for (int j = 1; j <= p.length(); j++) {
+        char pattern = p.charAt(j - 1);
+        dp[j][0] = dp[j - 1][0] && pattern == '*';
+        for (int i = 1; i <= s.length(); i++) {
+            char letter = s.charAt(i - 1);
+            if (pattern != '*') {
+                dp[j][i] = dp[j - 1][i - 1] && (pattern == '?' || pattern == letter);
+            } else
+                dp[j][i] = dp[j][i - 1] || dp[j - 1][i];
+        }
+    }
+    return dp[p.length()][s.length()];
+    
+class Solution:
+# @return a boolean
+def isMatch(self, s, p):
+    length = len(s)
+    if len(p) - p.count('*') > length:
+        return False
+    dp = [True] + [False]*length
+    for i in p:
+        if i != '*':
+            for n in reversed(range(length)):
+                dp[n+1] = dp[n] and (i == s[n] or i == '?')
+        else:
+            for n in range(1, length+1):
+                dp[n] = dp[n-1] or dp[n]
+        dp[0] = dp[0] and i == '*'
+    return dp[-1]
