36787d2ff500592eac6b1fbbce1de72a091e3187
UmassJin <jinblue.d@gmail.com>
2015-07-29 12:05:20 -0700

    Update and rename Uber面经集合.md to Uber面经集合.py

--- Experience/Uber面经集合.py
+++ Experience/Uber面经集合.py
@@ -1,3 +1,4 @@
+'''
 1. Given a list of words, find whether a new word is anagram of word in list
 2. Implement an LRU cache. 
 3. Using Object Oriented Design principles, design a method to check if a Sudoku board is valid (skeleton code was provided which was initially passed in through a 2-d array).  
@@ -6,3 +7,4 @@
 6. Implement LRU cache with get and set operations in constant time O(1). 
 7. 1. Segment Tree; 2. How to diagnose latency in data center;  
 8. Print all permutations of 3 pair of parens. ()()(), (()()), (())(),,,. etc 
+'''
