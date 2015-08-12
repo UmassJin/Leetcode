69d797566505a93216448ffe7e59b99e77391940
UmassJin <jinblue.d@gmail.com>
2015-08-11 17:54:04 -0700

    Update Strongly_Connect_Graph.py

--- Algorithm/Strongly_Connect_Graph.py
+++ Algorithm/Strongly_Connect_Graph.py
@@ -1,12 +1,17 @@
 # http://www.geeksforgeeks.org/strongly-connected-components/
 # http://www.geeksforgeeks.org/connectivity-in-a-directed-graph/
 '''
-Given a directed graph, find out whether the graph is strongly connected or not. A directed graph is strongly connected if there is a path between any two pair of vertices. For example, following is a strongly connected graph.
-`对于无向图来说判断连通非常简单，只需要做一次搜索，然后判断路径中是否经过了每个节点即可。但是有向图不可以这么做，比如上图，从节点0开始搜索可以经过所有节点，但是很明显它不是strongly connected。
-Naive的方法是，对每一个节点做一次DFS，如果存在一个节点在DFS中没有经过每一个节点，那么则不是强连通图。这个算法复杂度是O(V*(V+E))。
+Given a directed graph, find out whether the graph is strongly connected or not. 
+A directed graph is strongly connected if there is a path between any two pair of vertices. 
+For example, following is a strongly connected graph.
+`对于无向图来说判断连通非常简单，只需要做一次搜索，然后判断路径中是否经过了每个节点即可。
+但是有向图不可以这么做，比如上图，从节点0开始搜索可以经过所有节点，但是很明显它不是strongly connected。
+Naive的方法是，对每一个节点做一次DFS，如果存在一个节点在DFS中没有经过每一个节点，那么则不是强连通图。
+这个算法复杂度是O(V*(V+E))。
 或者用Floyd Warshall算法来找出任意两个节点的最短路径。复杂度是O(v3)。
 
-一个更好的办法是强连通分量算法Strongly Connected Components (SCC) algorithm。我们可以用O(V+E)时间复杂度找出一个图中所有的SCC。如果SCC只有一个，那么这个图就是强连通图。
+一个更好的办法是强连通分量算法Strongly Connected Components (SCC) algorithm。
+我们可以用O(V+E)时间复杂度找出一个图中所有的SCC。如果SCC只有一个，那么这个图就是强连通图。
 
 用Kosaraju算法，两个pass做DFS：
 
